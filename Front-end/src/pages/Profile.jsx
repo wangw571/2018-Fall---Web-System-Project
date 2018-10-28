a40283dff414cd93c8875c9ec567907c730de5e6
vishwaa.patel@mail.utoronto.ca <vishwaa.patel@mail.utoronto.ca>
2018-10-27 21:22:30 -0400

    Created some css blocks for the profile page

--- Front-end/src/pages/Profile.jsx
+++ Front-end/src/pages/Profile.jsx
@@ -5,6 +5,19 @@ export class Profile extends Component {
   render() {
     return <Page className='profile'>
       Profile
+      <form className="profile__form">
+        <div className="profile__organization-info">
+          Organization's name
+        </div>
+      </form>
+      
+
+
+
+
+
+
+
     </Page>
   }
 }
