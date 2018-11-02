0362c063dd6903c74d5b2aa2cf545bc88037888d
wangw571 <wang_w571@hotmail.com>
2018-11-02 15:49:53 -0400

    Unit test finished for account

--- testAccount.py
+++ testAccount.py
@@ -1,14 +1,16 @@
-import account
-
 import json
 
 from django.http import JsonResponse
 from django.http import HttpRequest
-import unittest
+from django.test import TestCase
 
-import sqlite3Operations
+from backend import account
+from backend import sqlite3Operations
 
-class AccountTestCase(unittest.TestCase):
+class AccountTestCase(TestCase):
+
+    def getJRvalue(self, jr: JsonResponse, key:str):
+        return json.loads(jr.content)[key]
 
     def setUp(self):
         account.createAccountsTable(account.accountDatabaseName)
@@ -31,63 +33,60 @@ class AccountTestCase(unittest.TestCase):
                 commit=True,
             )
 
-        def tearDown(self):
-            sqlite3Operations.run_query(
-                account.accountDatabaseName,
-                "DROP TABLE ACCOUNTS",
-                commit=True,
-            )
+    def tearDown(self):
+        sqlite3Operations.run_query(
+            account.accountDatabaseName,
+            "DROP TABLE ACCOUNTS",
+            commit=True,
+        )
 
-    def tokenGenerator(username:str, password:str) -> str:
-        return hashCoding(username+password)
+    def tokenGenerator(self, username:str, password:str) -> str:
+        return account.hashCoding(username+password)
 
 
     def test_login(self):
         jr1 = account.login("testuser1", "testpassword1")
-        self.assertEquals(tokenGenerator("testuser1", "testpassword1"), jr1.get("token"))
+        self.assertEquals(self.tokenGenerator("testuser1", "testpassword1"), self.getJRvalue(jr1, "token"))
         jr2 = account.login("testuser2", "testpassword2")
-        self.assertEquals(tokenGenerator("testuser2", "testpassword2"), jr2.get("token"))
+        self.assertEquals(self.tokenGenerator("testuser2", "testpassword2"), self.getJRvalue(jr2, "token"))
         jr3 = account.login("testuser1", "testpassword1")
-        self.assertEquals(tokenGenerator("testuser1", "testpassword1"), jr3.get("token"))
+        self.assertEquals(self.tokenGenerator("testuser1", "testpassword1"), self.getJRvalue(jr3, "token"))
         pass
 
     def test_login_failed(self):
         jr1 = account.login("testuser1", "falsepassword")
-        self.assertEquals("Incorrect Password Or Username", jr1.get("error"))
+        self.assertEquals("Incorrect Password Or Username", self.getJRvalue(jr1, "error"))
         jr2 = account.login("testuser2", "testpassword2")
-        self.assertEquals(tokenGenerator("testuser2", "testpassword2"), jr2.get("token"))
+        self.assertEquals(self.tokenGenerator("testuser2", "testpassword2"), self.getJRvalue(jr2, "token"))
         jr3 = account.login("testuser3", "falsepassword")
-        self.assertEquals("Incorrect Password Or Username", jr3.get("error"))
+        self.assertEquals("Incorrect Password Or Username", self.getJRvalue(jr3, "error"))
         pass
 
     def test_register(self):
         successMessage = "Creation successful"
         jr1 = account.registerAccount("testuserNewR", "testpasswordR", 0)
-        self.assertEquals(successMessage, jr1.get("success"))
+        self.assertEquals(successMessage, self.getJRvalue(jr1, "success"))
         jr2 = account.registerAccount("testuserNewR2", "testpasswordR", 2)
-        self.assertEquals(successMessage, jr2.get("success"))
+        self.assertEquals(successMessage, self.getJRvalue(jr2, "success"))
         pass
 
     def test_register_failed(self):
         successMessage = "Creation successful"
         failMessage = "Account Already Exist"
         jr1 = account.registerAccount("testuser1", "testpassword1", 0)
-        self.assertEquals(failMessage, jr1.get("error"))
+        self.assertEquals(failMessage, self.getJRvalue(jr1, "error"))
         jr2 = account.registerAccount("testuserNewR2", "testpasswordR", 2)
-        self.assertEquals(successMessage, jr2.get("success"))
+        self.assertEquals(successMessage, self.getJRvalue(jr2, "success"))
         jr3 = account.registerAccount("testuserNewR2", "testpasswordR", 0)
-        self.assertEquals(failMessage, jr3.get("error"))
+        self.assertEquals(failMessage, self.getJRvalue(jr3, "error"))
         jr4 = account.registerAccount("testuserNewR2", "testpasswordnotDup", 0)
-        self.assertEquals(failMessage, jr4.get("error"))
+        self.assertEquals(failMessage, self.getJRvalue(jr4, "error"))
         pass
 
 
     def test_existanceCheck(self):
         jr1 = account.existanceCheck("testuser1")
-        self.assertEquals(True, jr1.get("existance"))
+        self.assertEquals(True, self.getJRvalue(jr1, "existance"))
         jr2 = account.existanceCheck("testuserNewR2")
-        self.assertEquals(False, jr2.get("existance"))
+        self.assertEquals(False, self.getJRvalue(jr2, "existance"))
         pass
-
-if __name__ == "__main__":
-    unittest.main(exit=False)
