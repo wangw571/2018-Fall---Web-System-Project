7d83dcb051d08fef24f0004ae40893e24eb67baa
vishwaa.patel@mail.utoronto.ca <vishwaa.patel@mail.utoronto.ca>
2018-11-16 00:17:28 -0500

    Finished creating the interface for generating reports

--- Front-end/src/util/ReportsInfo.js
+++ Front-end/src/util/ReportsInfo.js
@@ -1,30 +1,144 @@
 // Private variables
 
 const reports = [
-    {
+  {
+    title: "Report1",
+    chart1: {
+        title: "Report1",
+        query: "data1",
+        column: "1",
+        id: "id",
+        template: "template"
+    },
+    chart2: {
       title: "Report1",
-      query: "data1"
+      query: "data1",
+      column: "1",
+      id: "id",
+      template: "template"
     },
-    {
-      title: "Report2",
-      query: "data2"
+    chart3: {
+      title: "Report1",
+      query: "data1",
+      column: "1",
+      id: "id",
+      template: "template"
     },
-    {
-      title: "Report3",
-      query: "data2"
-    }
+    chart4: {
+      title: "Report1",
+      query: "data1",
+      column: "1",
+      id: "id",
+      template: "template"
+    },
+    chart5: {
+      title: "Report1",
+      query: "data1",
+      column: "1",
+      id: "id",
+      template: "template"
+    },
+    chart6: {
+      title: "Report1",
+      query: "data1",
+      column: "1",
+      id: "id",
+      template: "template"
+    },
+  },
+  {
+    title: "Report2",
+    chart1: {
+        title: "Report1",
+        query: "data1",
+        column: "1",
+        id: "id",
+        template: "template"
+    },
+    chart2: {
+      title: "Report1",
+      query: "data1",
+      column: "1",
+      id: "id",
+      template: "template"
+    },
+    chart3: {
+      title: "Report1",
+      query: "data1",
+      column: "1",
+      id: "id",
+      template: "template"
+    },
+    chart4: {
+      title: "Report1",
+      query: "data1",
+      column: "1",
+      id: "id",
+      template: "template"
+    },
+    chart5: {
+      title: "Report1",
+      query: "data1",
+      column: "1",
+      id: "id",
+      template: "template"
+    },
+    chart6: {
+      title: "Report1",
+      query: "data1",
+      column: "1",
+      id: "id",
+      template: "template"
+    },
+  }
 ]
 
+/*const reports = [
+  {
+    title: "Report1"
+  },
+  {
+    title: "Report2"
+  }
+]*/
+
 const queries = [
-    {
-      name: "name1"
-    },
-    {
-      name: "name2"
-    },
-    {
-      name: "name3"
-    }
+  {
+    name: "name1",
+    id: "id1"
+  },
+  {
+    name: "name2",
+    id: "id2"
+  },
+  {
+    name: "name3",
+    id: "id3"
+  }
+]
+
+const templates = [
+  {
+    name: "template1",
+  },
+  {
+    name: "template2",
+  },
+  {
+    name: "template3"
+  }
+]
+
+const columns = [
+  {
+    name:"1",
+  },
+  {
+    name:"2",
+  },
+  {
+    name:"3"
+  }
 ]
 
 const data1 = [
@@ -69,12 +183,31 @@ class _ReportsInfo {
       return queries;
   }
 
-  getData = (active) => {
+  getColumns = () => {
+    return columns;
+  }
+
+  getTemplates = () => {
+    return templates;
+  }
+
+  getData = (query, template, column, queryID, title) => {
+    let active = 0;
     if (active === 0){
         return data1;
     } else {
         return data2;
     }
+  }
+
+  addReport = () => {
+    // add report to the database
+
+  }
+
+  deleteReport = (key) => {
+    // delete the data from the database
+
   }
 }
 
