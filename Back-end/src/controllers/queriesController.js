a310580603c5cb45b5c0275f31e12bc2ec083885
wangw571 <wang_w571@hotmail.com>
2018-11-18 06:49:31 -0500

    Some work on queries, started "run query"

--- Back-end/src/controllers/queriesController.js
+++ Back-end/src/controllers/queriesController.js
@@ -49,7 +49,7 @@ const getAllQueries = async (db, project) => (
 
 export const queriesController = {
   getQueries: async (res, req) => {
-    const { user: { sudo, _org } } = req;
+    const { user: { sudo } } = req;
     const db = await database.connect();
     let data = null;
 
@@ -68,7 +68,7 @@ export const queriesController = {
   },
 
   postQueries: async (res, req) => {
-    const { user: { sudo }, params: { query }, body } = req;
+    const { user: { sudo }, body } = req;
     let _id;
 
     // Check if super admin
@@ -78,7 +78,7 @@ export const queriesController = {
     }
 
     try {
-      _id = getObjectId(query);
+      _id = getObjectId(body.query);
     } catch (err) {
       res.status(401).json({ status: 'err', err });
       return
@@ -102,11 +102,11 @@ export const queriesController = {
   },
 
   getQuery: async (res, req) => {
-    const { user: { sudo }, params: { temp } } = req;
+    const { user: { sudo }, params } = req;
 
     // Get templates
     let queries = null;
-    const id = getObjectId(temp);
+    const id = getObjectId(params.qid);
     const db = await database.connect();
     if (sudo) {
       queries = await getAllQueries(db);
@@ -126,19 +126,12 @@ export const queriesController = {
   },
 
   postQuery: async (res, req) => {
-    const { user: { sudo }, params: { query }, body } = req;
-    let _id;
+    const { user: { sudo }, params, body } = req;
+    let _id = params.qid;
 
     // Check if super admin
     if (!sudo) {
       res.status(403).json({ status: 'error', err: 'Insufficient permission' });
-      return
-    }
-
-    try {
-      _id = getObjectId(query);
-    } catch (err) {
-      res.status(401).json({ status: 'err', err });
       return
     }
 
@@ -160,6 +153,34 @@ export const queriesController = {
   },
 
   runQuery: async (res, req) => {
-    
+    const { user: { sudo }, params } = req;
+    const ast = parser.parse('select * from dual');
+    res.status(200).json({ status: 'success', data: {ast, params} });
+    console.log(JSON.stringify(ast, null, 2));
+    return
+
+    // // Get templates
+    // let queries = null;
+    // const id = getObjectId(params.qid);
+    // const db = await database.connect();
+    // if (sudo) {
+    //   queries = await getAllQueries(db);
+    // } else {
+    //   res.status(403).json({ status: 'error', err: 'Insufficient permission' });
+    //   return
+    // }
+
+    // // Find template and return it
+    // const data = queries.filter(({ _id }) => _id.equals(id) );  
+    // if (data.length > 0) {
+    //   let TheQuery = data[0][3].toString();
+    //   let QuerySplitted = TheQuery.split(" ")
+
+
+    //   res.json({ status: 'success', data: data[0] });
+    // } else {
+    //   res.status(403).json({ status: 'error', err: "Query does not exist" });
+    // }
+    // db.close();
   }
 }
