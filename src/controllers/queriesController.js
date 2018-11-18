66f04572155d66a74f48382aa715ef4b104a2da1
wangw571 <wang_w571@hotmail.com>
2018-11-18 07:24:59 -0500

    included sql-to-js in package.json did some job on importing sql to js, todo: make the options to database

--- src/controllers/queriesController.js
+++ src/controllers/queriesController.js
@@ -154,33 +154,36 @@ export const queriesController = {
 
   runQuery: async (res, req) => {
     const { user: { sudo }, params } = req;
-    const ast = parser.parse('select * from dual');
-    res.status(200).json({ status: 'success', data: {ast, params} });
-    console.log(JSON.stringify(ast, null, 2));
-    return
 
-    // // Get templates
-    // let queries = null;
-    // const id = getObjectId(params.qid);
-    // const db = await database.connect();
-    // if (sudo) {
-    //   queries = await getAllQueries(db);
-    // } else {
-    //   res.status(403).json({ status: 'error', err: 'Insufficient permission' });
-    //   return
-    // }
+    // Get queries
+    let queries = null;
+    const id = getObjectId(params.qid);
+    const db = await database.connect();
+    if (sudo) {
+      queries = await getAllQueries(db);
+    } else {
+      res.status(403).json({ status: 'error', err: 'Insufficient permission' });
+      return
+    }
 
-    // // Find template and return it
-    // const data = queries.filter(({ _id }) => _id.equals(id) );  
-    // if (data.length > 0) {
-    //   let TheQuery = data[0][3].toString();
-    //   let QuerySplitted = TheQuery.split(" ")
-
-
-    //   res.json({ status: 'success', data: data[0] });
-    // } else {
-    //   res.status(403).json({ status: 'error', err: "Query does not exist" });
-    // }
-    // db.close();
+    // Find the required template
+    const data = queries.filter(({ _id }) => _id.equals(id) );  
+    if (data.length > 0) {
+      let TheQuery = data[0][3].toString();
+      const parser = require('js-sql-parser');
+      let parsedQuery;
+      // Try translate the sql query to dictionary of options
+      try {
+        parsedQuery = parser.parse(TheQuery);
+      } catch (err) {
+        res.status(401).json({ status: 'err', err});
+        return
+      }
+        // TODO: query job
+      res.json({ status: 'success', data: data[0] });
+    } else {
+      res.status(403).json({ status: 'error', err: "Query does not exist" });
+    }
+    db.close();
   }
 }
