ca882776339fe1d3a49d5b771759df24fa84e065
wangw571 <wang_w571@hotmail.com>
2018-11-18 23:42:20 -0500

    More Query Tests Added delete in query

--- src/test/queries/queries.qid.test.js
+++ src/test/queries/queries.qid.test.js
@@ -2,75 +2,171 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import { request } from '../util';
 const should = chai.should();
+let qid1, qid2, qidtemp;
 
 chai.use(chaiHttp);
 describe('users.js - users', () => {
 
-  describe('/', () => {
-    describe('GET', () => {
+    // Setup testing queries
+    before(() =>
+        new Promise(resolve => {
+            request(`/queries`, 'POST')
+                .asSystemAdmin()
+                .send({ name: "testQ1", query: '[{ $match: { status: "A" } },{ $group: { _id: "$cust_id", total: { $sum: "$amount" } } },{ $sort: { total: -1 } }]' })
+                .end((err, res) => {
+                    qid1 = res.body._id;
+                });
+            request(`/queries`, 'POST')
+                .asSystemAdmin()
+                .send({ name: "testQ2", query: '[{ $match: { status: "A" } },{ $group: { _id: "$cust_id", total: { $sum: "$amount" } } },{ $sort: { total: -1 } }]' })
+                .end((err, res) => {
+                    qid2 = res.body._id;
+                });
+            resolve();
+        })
+    );
 
-      describe('As regular organization', () => {
-        it('it should return error', done => {
-          request('/users')
-          .asRegularAdmin()
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('status', 'success');
-            res.body.data.should.be.a('array').length(2);
-            done();
-          });
+    // Remove testing queries
+    after(() =>
+        new Promise(resolve => {
+            request(`/queries/${qid1}`, 'DELETE')
+                .asSystemAdmin()
+                .end((err, res) => {
+                    resolve();
+                });
+            request(`/queries/${qid2}`, 'DELETE')
+                .asSystemAdmin()
+                .end((err, res) => {
+                    resolve();
+                });
+        })
+    );
+
+    describe('/qid', () => {
+        describe('GET', () => {
+
+            describe('As from regular organization', () => {
+                it('it should return error', done => {
+                    request(`/queries/${qid1}`)
+                        .asRegularAdmin()
+                        .end((err, res) => {
+                            res.should.have.status(403);
+                            res.body.should.be.a('object');
+                            res.body.should.have.property('status', 'error');
+                            res.body.should.have.property('err', 'Insufficient permission');
+                            done();
+                        });
+                });
+            });
+
+            describe('As system admin', () => {
+                it('it should return success', done => {
+                    request(`/queries/${qid1}`)
+                        .asSystemAdmin()
+                        .end((err, res) => {
+                            res.should.have.status(200);
+                            res.body.should.be.a('object');
+                            res.body.should.have.property('status', 'success');
+                            res.body.should.have.property('data');
+                            console.log(res.body.data);
+                            done();
+                        });
+                });
+            });
+
         });
-      });
+        describe('DELETE', () => {
 
-      describe('As system organization', () => {
-        it('it should return error', done => {
-          request('/users')
-          .asSystemAdmin()
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('status', 'success');
-            res.body.data.should.be.a('array').length(4);
-            done();
-          });
+            describe('As from regular organization', () => {
+                it('it should return error', done => {
+                    request(`/queries/${qid1}`, "DELETE")
+                        .asRegularAdmin()
+                        .end((err, res) => {
+                            res.should.have.status(403);
+                            res.body.should.be.a('object');
+                            res.body.should.have.property('status', 'error');
+                            res.body.should.have.property('err', 'Insufficient permission');
+                            done();
+                        });
+                });
+            });
+
+            describe('As system admin', () => {
+                it('it should return success', done => {
+                    request(`/queries/${qid1}`, "DELETE")
+                        .asSystemAdmin()
+                        .end((err, res) => {
+                            res.should.have.status(200);
+                            res.body.should.be.a('object');
+                            res.body.should.have.property('status', 'success');
+                            res.body.should.have.property('data');
+                            console.log(res.body.data);
+                            done();
+                        });
+                });
+            });
+
+            describe('As system admin, deleting non-existed query', () => {
+                it('it should return error', done => {
+                    request(`/queries/notExisted`, "DELETE")
+                        .asSystemAdmin()
+                        .end((err, res) => {
+                            res.should.have.status(401);
+                            res.body.should.be.a('object');
+                            res.body.should.have.property('status', 'error');
+                            res.body.should.have.property('err');
+                            done();
+                        });
+                });
+            });
         });
-      });
 
+        describe('POST', () => {
+
+            describe('As from regular organization', () => {
+                it('it should return error', done => {
+                    request(`/queries/${qid1}`, "POST")
+                        .asRegularAdmin()
+                        .send({ name: "qTestSt" })
+                        .end((err, res) => {
+                            res.should.have.status(403);
+                            res.body.should.be.a('object');
+                            res.body.should.have.property('status', 'error');
+                            res.body.should.have.property('err', 'Insufficient permission');
+                            done();
+                        });
+                });
+            });
+
+            describe('As system admin', () => {
+                it('it should return success', done => {
+                    request(`/queries/${qid1}`, "POST")
+                        .asSystemAdmin()
+                        .send({ query: '[{ $match: { status: "AAA" } }]' })
+                        .end((err, res) => {
+                            res.should.have.status(200);
+                            res.body.should.be.a('object');
+                            res.body.should.have.property('status', 'success');
+                            res.body.should.have.property('data');
+                            console.log(res.body.data);
+                            done();
+                        });
+                });
+            });
+            describe('As system admin, edit non-existed query', () => {
+                it('it should return error', done => {
+                    request(`/queries/notExisted`, "POST")
+                        .asSystemAdmin()
+                        .send({ query: '[{ $match: { status: "AAA" } }]' })
+                        .end((err, res) => {
+                            res.should.have.status(401);
+                            res.body.should.be.a('object');
+                            res.body.should.have.property('status', 'error');
+                            res.body.should.have.property('err');
+                            done();
+                        });
+                });
+            });
+        });
     });
-  });
-
-  describe('/:org', () => {
-    describe('GET', () => {
-
-      describe('As different organization', () => {
-        it('it should return error', done => {
-          request(`/users/${process.env.SYS_ORG}`)
-          .asRegularAdmin()
-          .end((err, res) => {
-            res.should.have.status(403);
-            res.body.should.be.a('object');
-            res.body.should.have.property('status', 'error');
-            res.body.should.have.property('err');
-            done();
-          });
-        });
-      });
-
-      describe('As same organization', () => {
-        it('it should return error', done => {
-          request(`/users/${process.env.SYS_ORG}`)
-          .asSystemAdmin()
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('status', 'success');
-            res.body.data.should.be.a('array').length(2);
-            done();
-          });
-        });
-      });
-
-    });
-  });
 });
