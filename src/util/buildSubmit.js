94e455d9a433840b67b99818a09f24eee43c6f16
Frederic Pun <frederic.pun@gmail.com>
2018-11-26 02:36:33 -0500

    Inverted check

--- src/util/buildSubmit.js
+++ src/util/buildSubmit.js
@@ -17,9 +17,9 @@ export const buildSubmit = async (req, res, next) => {
     res.status(401).json({ status: 'error', err: 'Missing file' });
     return
   }
-  
-  if(/.xlsx$/.exec(file.originalname)){
-    res.status(415).json({ status: ERROR, err: `Template extension format not supported, please use xlsx format files` });
+
+  if(!/.xlsx$/.exec(file.originalname)){
+    res.status(415).json({ status: 'error', err: 'Template extension format not supported, please use xlsx format files' });
     return
   }
 
